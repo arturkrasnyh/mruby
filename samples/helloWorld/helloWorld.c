41e00b02fed4aebb4fceb0d687d5e3a232a8d0a5
Matt Aimonetti <mattaimonetti@gmail.com>
2012-04-25 09:59:36 +0200

    basic hello world example

---
+++ samples/helloWorld/helloWorld.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Include the mruby header */
+#include <mruby.h>
+#include <mruby/proc.h>
+#include <mruby/data.h>
+// to be replaced by #include <mruby/compile.h>
+#include <compile.h>
+
+int main(void)
+{
+  struct mrb_parser_state *p;
+  mrb_state *mrb = mrb_open();
+  char code[] = "p 'hello world!'";
+  printf("Executing code with mruby!\n");
+
+  p = mrb_parse_string(mrb, code);
+  int n;
+  n = mrb_generate_code(mrb, p->tree);
+  mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_top_self(mrb));
+  if (mrb->exc) {
+   mrb_funcall(mrb, mrb_nil_value(), "p", 1, mrb_obj_value(mrb->exc));
+  }
+  return 0;
+}
